718a1c1730cf0807875f4c6d4fd7d7ce6adcca8c
arpan-khandelwal <akpalwal@gmail.com>
2020-03-28 17:08:50 +0530

    main class done

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,5 +7,38 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyPriorityQueue priorityQueue = new MyPriorityQueue();
+        System.out.println("Enter number of nodes ");/*--->*/
+        int nodes = sc.nextInt();
+        while (nodes-- > 0) {
+            Student student = new Student();
+            System.out.println("Enter name ");/*--->*/
+            String name = sc.next().trim();
+            System.out.println("Enter roll number");/*--->*/
+            int roll = sc.nextInt();
+            student.setName(name);
+            student.setRollNumber(roll);
+
+            Node node = new Node();
+            node.setStudent(student);
+
+            priorityQueue.enQueue(node);
+        }
+
+        System.out.println("Displaying Queue");
+
+        Node node = priorityQueue.deQueue();
+        while (node != null) {
+            System.out.println(node);
+            node = priorityQueue.deQueue();
+        }
+    }
 }
